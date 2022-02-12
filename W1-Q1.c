5e57ba11dbb16d7a8542492de03078427b8d4373
KKundan2003 <99540149+KKundan2003@users.noreply.github.com>
2022-02-12 22:01:38 +0530

    Week 1 solutions

---
+++ W1-Q1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+
+printf("NAME:K KUNDAN KUMAR SUBUDHI\n");
+printf("REGD.NUMBER:2102070088\n");
+printf("BRANCH:ELECTRONICS AND TELECOMMUNICATION\n");
+printf("HOBBIES:SKETCHING\n");
+return 0;
+
+}
